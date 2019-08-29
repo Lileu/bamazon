ff35d56ed2850dd2dfbc495b4547d2d0fb7be0ec
Lisa Leu <lis.leu@gmail.com>
2019-08-30 08:13:39 +1000

    added db code

--- departments.sql
+++ departments.sql
@@ -11,3 +11,15 @@
 -- 2. Modify the products table so that there's a product_sales column, and modify your `bamazonCustomer.js` app so that when a customer purchases anything from the store, the price of the product multiplied by the quantity purchased is added to the product's product_sales column.
 
 --    * Make sure your app still updates the inventory listed in the `products` column.
+ DROP DATABASE IF EXISTS bamazon_db;
+
+ CREATE DATABASE bamazon_db;
+
+ USE bamazon_db;
+
+ CREATE TABLE departments (
+     department_id INT NOT NULL AUTO_INCREMENT,
+     department_name VARCHAR(40),
+     over_head_costs INT,
+     PRIMARY KEY (department_id)
+ )
